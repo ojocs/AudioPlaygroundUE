7e7c207263da4c9b16ed70ed3df1a3265a11a399
lajas <35551771+ojocs@users.noreply.github.com>
2024-02-18 22:53:14 -0600

    C++ continued

--- Source/AudioSynesthesiaTest/CubesSpawner.cpp
+++ Source/AudioSynesthesiaTest/CubesSpawner.cpp
@@ -9,6 +9,8 @@ ACubesSpawner::ACubesSpawner()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CubesClockName = "QuartzCubesClock";
+	// Init subsystem
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +18,22 @@ void ACubesSpawner::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	// go through elements in pool set them up
+	for (int32 i = 0; i < PoolSize; ++i)
+	{
+		UAudioComponent* audioSourceComponent = NewObject<UAudioComponent>(this);
+		audioSourceComponent->RegisterComponent();
+		audioSourceComponent->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+		audioSourcePool.Add(audioSourceComponent);
+	}
+	
+	// Set up clock to spawn cubes on metronome quantization events
+	//CubesClock = GetWorld()->GetGameInstance()->GetSubsystem<UQuartzSubsystem>()->
+	//	CreateNewClock(GetWorld(), CubesClockName, QuartzClockSettings);
+
+	//QuartzMetronomeEvent.BindUFunction(GetWorld(), FName(TEXT("OnQuartzQuantizationEvents")));
+
+	//CubesClock->SubscribeToAllQuantizationEvents(GetWorld(), QuartzMetronomeEvent, CubesClock);
 }
 
 // Called every frame
@@ -23,5 +41,20 @@ void ACubesSpawner::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+//void ACubesSpawner::OnQuartzQuantizationEvents_Implementation(FName ClockName, EQuartzCommandQuantization QuantizationType, int32 NumBars, int32 Beat, float BeatFraction)
+//{
+//	switch (QuantizationType)
+//	{
+//	case EQuartzCommandQuantization::Beat:
+//		SpawnAudioSource();
+//		break;
+//	};
+//}
+
+void ACubesSpawner::SpawnAudioSource_Implementation()
+{
+		
 }
 
--- Source/AudioSynesthesiaTest/CubesSpawner.h
+++ Source/AudioSynesthesiaTest/CubesSpawner.h
@@ -3,8 +3,14 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Quartz/QuartzSubsystem.h"
+#include "Quartz/AudioMixerClockHandle.h"
+#include "Sound/QuartzQuantizationUtilities.h"
 #include "GameFramework/Actor.h"
+#include "Sound/SoundBase.h"
+#include "Components/AudioComponent.h"
 #include "CubesSpawner.generated.h"
+
 
 UCLASS()
 class AUDIOSYNESTHESIATEST_API ACubesSpawner : public AActor
@@ -23,4 +29,59 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+#pragma region Resource Pools
+	// Array of sounds
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio")
+	TArray<USoundBase*> soundsCollection;
+
+	// Count of "speakers" 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Audio", meta = (ClampMin = "1", UIMin = "1"))
+	int32 PoolSize;
+#pragma endregion
+
+#pragma region Spawn Logic
+	// Audio Source Spawning Logic
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void SpawnAudioSource();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	float CustomSpawnTime = 5.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	float MinSpawnRadius = 100.0f;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Spawning")
+	float MaxSpawnRadius = 500.0f;
+#pragma endregion
+
+#pragma region Clock
+public:
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "QuartzClock")
+	//UQuartzClockHandle* CubesClock;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "QuartzClock")
+	FName CubesClockName;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "QuartzClock")
+	//FQuartzClockSettings QuartzClockSettings;
+//
+//	/*UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+//	void OnQuartzQuantizationEvents(FName ClockName, EQuartzCommandQuantization QuantizationType, int32 NumBars, int32 Beat, float BeatFraction);*/
+private:
+//	FOnQuartzMetronomeEventBP QuartzMetronomeEvent;
+//
+//	// Clock
+//	FTimerHandle TimerHandle;
+
+#pragma endregion
+
+private:
+#pragma region Resource Pools
+	// The "speakers" themselves
+	TArray<UAudioComponent*> audioSourcePool;
+
+	// Function to get an available "speaker" from the source pool
+	UAudioComponent* GetAvailableAudioSourceComponent();
+#pragma endregion
 };
